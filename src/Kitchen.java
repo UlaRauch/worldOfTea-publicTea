a67e42d9c6591d597c1ee8815650b657065e83b4
UlaRauch <71543978+UlaRauch@users.noreply.github.com>
2021-12-02 22:45:52 +0100

    cleaned up the Kitchen

--- src/Kitchen.java
+++ src/Kitchen.java
@@ -5,14 +5,17 @@ public class Kitchen {
 
     public static void main(String[] args) {
 
-        //You need a teapot! Take care to use the right temperature
-        Teapot teapot = new Teapot();
-         //Of course you can't make tea without a tea bag *
-        teapot.chooseTeaBag();
-         // make yourself a cup of tea
-        Cup cupOfTea = new Cup(teapot.makeTea(3, 100));
-         //This cup does not exist and if it would exist, it would be empty. So go, make some tea!
-         cupOfTea.content.drinkTea();
+        // You need a teapot!
+
+        // Of course you can't make tea without a tea bag *
+
+        // make yourself a cupOfTea
+
+        // This cup does not exist yet
+        cupOfTea.content.drinkTea();
+
+        //check how many cups you've already had. Maybe it's time to stop?
+
     }
 }
-// * yes, smartypants, you can. but loose tea does not exist in this exercise. go and find out why ;)
+// * yes, smartypants, you can. but loose tea does not exist in this exercise. go and find out :)
