2c0d695ea269225a210b452c27c49e82c5d8265f
UlaRauch <71543978+UlaRauch@users.noreply.github.com>
2021-12-03 00:11:51 +0100

    added kitchen tasks

--- src/Kitchen.java
+++ src/Kitchen.java
@@ -5,17 +5,19 @@ public class Kitchen {
 
     public static void main(String[] args) {
 
+        // 1. You need a teapot!
 
-        // You need a teapot!
+        // 2. Of course, you can't make tea without a tea bag *
 
-        // Of course you can't make tea without a tea bag *
+        // 3. make yourself a cupOfTea (take care to use the right temperature and brewing time!)
 
-        // make yourself a cupOfTea
-
-        // This cup does not exist yet
+        // 4. This cup does not exist yet
         cupOfTea.content.drinkTea();
 
-        //check how many cups you've already had. Maybe it's time to stop?
+        // 5. check how many cups you've already had. Maybe it's time to stop?
+
+        // 6. We want some privacy!
+        // Go and try what you can hide from the public (in a way that you can still enjoy your tea)
 
     }
 }
